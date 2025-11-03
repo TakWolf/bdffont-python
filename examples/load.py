aaef02190cc985351581f2dfdd759cf43179fd1e
TakWolf <takwolf@foxmail.com>
2025-11-03 14:44:30 +0800

    升级 unifont

--- examples/load.py
+++ examples/load.py
@@ -10,7 +10,7 @@ def main():
         shutil.rmtree(outputs_dir)
     outputs_dir.mkdir(parents=True)
 
-    font = BdfFont.load(assets_dir.joinpath('unifont', 'unifont-17.0.01.bdf'))
+    font = BdfFont.load(assets_dir.joinpath('unifont', 'unifont-17.0.03.bdf'))
     print(f'name: {font.name}')
     print(f'size: {font.point_size}')
     print(f'ascent: {font.properties.font_ascent}')
@@ -26,7 +26,7 @@ def main():
             text = ''.join('  ' if color == 0 else '██' for color in bitmap_row)
             print(f'{text}*')
         print()
-    font.save(outputs_dir.joinpath('unifont-17.0.01.bdf'))
+    font.save(outputs_dir.joinpath('unifont-17.0.03.bdf'))
 
 
 if __name__ == '__main__':
